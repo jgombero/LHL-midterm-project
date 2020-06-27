45dbb867be416365b2d3f533c61424ee77f49696
Joshua Gomberoff <joshuagomberoff@gmail.com>
2020-06-27 21:09:08 +0000

    Empty files for project structure

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const morgan     = require('morgan');
 
 // PG database client/connection setup
 const { Pool } = require('pg');
-const dbParams = require('./lib/db.js');
+const dbParams = require('../lib/db.js');
 const db = new Pool(dbParams);
 db.connect();
 
@@ -33,8 +33,8 @@ app.use(express.static("public"));
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
-const usersRoutes = require("./routes/users");
-const widgetsRoutes = require("./routes/widgets");
+const usersRoutes = require("../routes/users");
+const widgetsRoutes = require("../routes/widgets");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
