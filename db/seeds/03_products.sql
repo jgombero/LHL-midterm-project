345da4f949c4fc1ac458b53441b0d1fe82bc0158
alex-hladun <alexhladun@gmail.com>
2020-06-27 05:15:22 +0000

    Added seeds

--- db/seeds/03_products.sql
+++ db/seeds/03_products.sql
@@ -3,5 +3,8 @@ VALUES (1, 'Ford F150', 'A really nice truck', 3000000, 3, 'https://i.imgur.com/
 (1, 'Rake', 'The best tool for cleaning your yard. $100 OBO', 10000, 5, 'https://i.imgur.com/J9NxVYb.jpg'),
 (1, 'Used Laptop', 'Doesn''t work but it''s yours for CHEAP!', 100, 1,'https://i.imgur.com/eM2IwIw.jpg'),
 (1, 'Mercedes Benz SLS AMG Ride on car', 'Excellent condition! High quality. Suitable For Child With Age From 24 months to 5 years.', 20000, 4, 'https://i.imgur.com/UfTbiCM.jpg'),
-(1, 'Limestone 40mm', 'This stuff rocks!', 1000, 2,'https://i.imgur.com/H3hcZU7.jpg');
-
+(1, 'Limestone 40mm', 'This stuff rocks!', 1000, 2,'https://i.imgur.com/H3hcZU7.jpg'),
+(2, '1995 Honda Civic', 'Great car! Got me around the block for a couple years but my gf is too embarassed to be seen in it', 50000, 3, 'https://i.imgur.com/H3hcZU7.jpg'),
+(3, 'Farm Fresh Eggs!!', 'These eggs are delicious', 1000, 8, 'https://i.imgur.com/P9Yb4Gw.jpg'),
+(2, 'Free BBQ!', 'My grilling days are over. Come pick up, I will absolutely NOT deliver this to you', 0, 8, 'https://i.imgur.com/oegkz23.jpg'),
+(3, 'Large, Heavy Mirror', 'This thing is seriously heavy', 2000, 8, 'https://i.imgur.com/HiuF6D0.jpg');
