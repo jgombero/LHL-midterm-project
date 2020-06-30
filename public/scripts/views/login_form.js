f24db95ffc78ba4e54eaea0ee445f8d08ac29f50
alex-hladun <alexhladun@gmail.com>
2020-06-30 21:20:34 +0000

    Login form working

--- public/scripts/views/login_form.js
+++ public/scripts/views/login_form.js
@@ -1,18 +1,15 @@
-$(() => {
+const $logInForm = $(`<h1>Log In!</h1>
+<form id="login-form" method="POST" action="/users/login">
+  <label for="user_email"></label>
+  <input type="email" name="user_email" placeholder="Email Address">
+  <label for="user_password"></label>
+  <input type="password" name="user_password" placeholder="Password>
+  <button type-"submit">
+    Log In
+  </button>
+</form>`);
 
-  const $logInForm =
-  `
-    <h1>Log In!</h1>
-    <form id="login-form" method="POST" action="/users/login">
-      <label for="user_email"></label>
-      <input type="email" name="user_email" placeholder="Email Address">
-      <label for="user_password"></label>
-      <input type="password" name="user_password" placeholder="Password>
-      <button type-"submit">
-        Log In
-      </button>
-    </form>
-  `;
+$(() => {
 
   $('#login-form').on('submit', function(event) {
     event.preventDefault();
--- public/scripts/views_manager.js
+++ public/scripts/views_manager.js
@@ -2,18 +2,18 @@
 
 
 // Not too sure if right, but trying to setup views_manager
-const $section = $('#main-container');
 
 window.views_manager = {};
 
 window.views_manager.show = function(item) {
 
+  const $section = $('main');
   const $products = $('#main-container');
   $products.detach();
   $logInForm.detach();
-  $signUpForm.detach();
-  $messages.detach();
-  $newProduct.detach();
+  // $signUpForm.detach();
+  // $messages.detach();
+  // $newProduct.detach();
 
   switch(item) {
     case 'product':
