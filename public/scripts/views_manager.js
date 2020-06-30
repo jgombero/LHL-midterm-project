0d378c199aa574541465e2e9dbfd100f4e743ee6
Joshua Gomberoff <joshuagomberoff@gmail.com>
2020-06-30 19:13:33 +0000

    uncommented switch statement

--- public/scripts/views_manager.js
+++ public/scripts/views_manager.js
@@ -15,34 +15,34 @@ $(() => {
   });
 
 // Not too sure if right, but trying to setup views_manager
-// const $section = $('#products');
+const $section = $('#main-container');
 
-// window.views_manager = {};
+window.views_manager = {};
 
-// window.views_manager.show = function(item) {
-//   $products.detach();
-//   $logInForm.detach();
-//   $signUpForm.detach();
-//   $messages.detach();
-//   $newProduct.detach();
+window.views_manager.show = function(item) {
+  $products.detach();
+  $logInForm.detach();
+  $signUpForm.detach();
+  $messages.detach();
+  $newProduct.detach();
 
-//   switch(item) {
-//     case 'product':
-//       $product.appendTo($section);
-//       break;
-//     case 'newProduct':
-//       $newProduct.appendTo($section);
-//       break;
-//     case 'logIn':
-//       $logInForm.appendTo($section);
-//       break;
-//     case 'signUp':
-//       $signUpForm.appendTo($section);
-//       break;
-//     case 'messages':
-//       $messages.appendTo($section);
-//       break;
-//   }
-// }
+  switch(item) {
+    case 'product':
+      $product.appendTo($section);
+      break;
+    case 'newProduct':
+      $newProduct.appendTo($section);
+      break;
+    case 'logIn':
+      $logInForm.appendTo($section);
+      break;
+    case 'signUp':
+      $signUpForm.appendTo($section);
+      break;
+    case 'messages':
+      $messages.appendTo($section);
+      break;
+  }
+}
 });
 
