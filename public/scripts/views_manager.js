b3d75868e21ceb085c66e538bf04ae4ca82afc99
Joshua Gomberoff <joshuagomberoff@gmail.com>
2020-06-30 04:59:51 +0000

    Started to code this file

--- public/scripts/views_manager.js
+++ public/scripts/views_manager.js
@@ -14,6 +14,35 @@ $(() => {
     });
   });
 
+// Not too sure if right, but trying to setup views_manager
+// const $section = $('#products');
 
+// window.views_manager = {};
+
+// window.views_manager.show = function(item) {
+//   $products.detach();
+//   $logInForm.detach();
+//   $signUpForm.detach();
+//   $messages.detach();
+//   $newProduct.detach();
+
+//   switch(item) {
+//     case 'product':
+//       $product.appendTo($section);
+//       break;
+//     case 'newProduct':
+//       $newProduct.appendTo($section);
+//       break;
+//     case 'logIn':
+//       $logInForm.appendTo($section);
+//       break;
+//     case 'signUp':
+//       $signUpForm.appendTo($section);
+//       break;
+//     case 'messages':
+//       $messages.appendTo($section);
+//       break;
+//   }
+// }
 });
 
