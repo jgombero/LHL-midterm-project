03d010b57834ded9937897a3a9fbfe842c86b2f4
Joshua Gomberoff <joshuagomberoff@gmail.com>
2020-06-30 19:11:26 +0000

    submit listener for login

---
+++ public/scripts/views/login_form.js
@@ -0,0 +1,31 @@
+$(() => {
+
+  const $logInForm =
+  `
+    <h1>Log In!</h1>
+    <form id="login-form" method="POST" action="/users/login">
+      <label for="user_email"></label>
+      <input type="email" name="user_email" placeholder="Email Address">
+      <label for="user_password"></label>
+      <input type="password" name="user_password" placeholder="Password>
+      <button type-"submit">
+        Log In
+      </button>
+    </form>
+  `;
+
+  $('#login-form').on('submit', function(event) {
+    event.preventDefault();
+
+    const data = $(this).serialize();
+    console.log(data);
+    login(data)
+    .then(json => {
+      console.log(json);
+      views_manager.show('products');
+    });
+  });
+
+});
+
+
