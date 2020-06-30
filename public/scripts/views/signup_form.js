71427ef5fe1dbae146b386033890a838e894edb9
Joshua Gomberoff <joshuagomberoff@gmail.com>
2020-06-30 19:11:45 +0000

    incomplete form

---
+++ public/scripts/views/signup_form.js
@@ -0,0 +1,18 @@
+$(() => {
+
+  const $SignUpForm =
+  `
+    <h1>Sign Up!</h1>
+    <form method="POST" action="/users">
+      <label for="user_email"></label>
+      <input type="email" name="user_email" placeholder="Email Address">
+      <label for="user_password"></label>
+      <input type="password" name="user_password" placeholder="Password>
+      <button type-"submit">
+        Sign Up
+      </button>
+    </form>
+  `;
+
+
+});
