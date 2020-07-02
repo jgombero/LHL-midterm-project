92b8dda4655a72afc74a4f8f34a039f7ff5250bc
Joshua Gomberoff <joshuagomberoff@gmail.com>
2020-07-02 05:10:03 +0000

    Added delete on cascade

--- db/schema/05_messages.sql
+++ db/schema/05_messages.sql
@@ -4,7 +4,7 @@ CREATE TABLE messages (
   id SERIAL PRIMARY KEY NOT NULL,
   from_user_id INTEGER REFERENCES users(id),
   to_user_id INTEGER REFERENCES users(id),
-  product_id INTEGER REFERENCES products(id),
+  product_id INTEGER REFERENCES products(id) ON DELETE CASCADE,
   message_text TEXT,
   time TIMESTAMP DEFAULT NOW()::timestamp
 );
